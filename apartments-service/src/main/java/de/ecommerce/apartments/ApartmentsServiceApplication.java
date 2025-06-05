f8f73b55ffd4de0310899734f49802b603508507
Egor Busuioc <busuiocegor@gmail.com>
2025-06-05 22:07:13 +0200

    Apartments service init.

---
+++ apartments-service/src/main/java/de/ecommerce/apartments/ApartmentsServiceApplication.java
@@ -0,0 +1,13 @@
+package de.ecommerce.apartments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentsServiceApplication.class, args);
+	}
+
+}
---
+++ apartments-service/src/test/java/de/ecommerce/apartments/ApartmentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ecommerce.apartments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
