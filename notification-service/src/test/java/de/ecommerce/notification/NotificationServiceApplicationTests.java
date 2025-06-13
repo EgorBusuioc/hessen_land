ead9ef471e210258d853269b9cbe190976f45540
Egor Busuioc <busuiocegor@gmail.com>
2025-06-13 17:26:53 +0200

    Add initial notification service with application properties and basic structure

---
+++ notification-service/src/test/java/de/ecommerce/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ecommerce.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
